8a58aedd6d9e1556f93a22ccbce309fa8875f255
dmizr <david.mizrahi@epfl.ch>
2020-08-01 12:29:17 +0200

    Handle sky drop case in preparing moves

--- src/poke_env/environment/abstract_battle.py
+++ src/poke_env/environment/abstract_battle.py
@@ -287,10 +287,13 @@ class AbstractBattle(ABC):
             pokemon = split_message[2]
             self.get_pokemon(pokemon).must_recharge = True
         elif split_message[1] == "-prepare":
-            attacker, move, defender = split_message[2:5]
-            # TODO handle sky drop case
-            # TODO store the slot targeted, not the target
-            self.get_pokemon(attacker)._prepare(move, self.get_pokemon(defender))
+            attacker, move = split_message[2:4]
+            if to_id_str(move) == "skydrop":
+                defender = split_message[4]
+                self.get_pokemon(defender)._start_effect("Sky Drop")
+            else:
+                defender = None
+            self.get_pokemon(attacker)._prepare(move, defender)
         elif split_message[1] == "-primal":
             pokemon = split_message[2]
             self.get_pokemon(pokemon)._primal()
